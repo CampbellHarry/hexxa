43390b045196212dd447e27bafd9840abeff261f
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2023-11-08 13:25:50 +0000

    Security Update: ratelimit

--- backend/sell.js
+++ backend/sell.js
@@ -3,6 +3,22 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+app.get('/:path', function(req, res) {
+  let path = req.params.path;
+  if (isValidPath(path))
+    res.sendFile(path);
+});
+
 app.use(bodyParser.json());
 
 app.post('/submit', (req, res) => {
