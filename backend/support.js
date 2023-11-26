303e9868fdaf1b79cee01b24f31fd53b3f69536b
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2023-11-26 22:49:48 +0000

    added support.html

---
+++ backend/support.js
@@ -0,0 +1,47 @@
+    document.querySelector('.boxxa').addEventListener('click', () => {
+        deliverysupport();
+    });
+
+    function deliverysupport() {
+        window.open('/deliverysupport', '_self');
+    }
+
+    document.querySelector('.pay').addEventListener('click', () => {
+        paymentsupport();
+    });
+
+    function paymentsupport() {
+        window.open('/paymentsupport', '_self');
+    }
+
+    document.querySelector('.acc').addEventListener('click', () => {
+        accountsupport();
+    });
+
+    function accountsupport() {
+        window.open('/accountsupport', '_self');
+    }
+
+    document.querySelector('.prem').addEventListener('click', () => {
+        premiumsupport();
+    });
+
+    function premiumsupport() {
+        window.open('/premiumsupport', '_self');
+    }
+
+    document.querySelector('.rep').addEventListener('click', () => {
+        reportsupport();
+    });
+
+    function reportsupport() {
+        window.open('/reportsupport', '_self');
+    }
+
+    document.querySelector('.mod').addEventListener('click', () => {
+        modsupport();
+    });
+
+    function modsupport() {
+        window.open('/modsupport', '_self');
+    }
